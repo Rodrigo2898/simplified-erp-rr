3e5597dfb171e6217267d82b2fde148ab61b5cca
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-02-19 22:06:02 -0300

    Estrutura inicial do projeto

---
+++ pessoa-service/src/main/java/com/ms/rr/pessoa_service/PessoaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.rr.pessoa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaServiceApplication.class, args);
+	}
+
+}
