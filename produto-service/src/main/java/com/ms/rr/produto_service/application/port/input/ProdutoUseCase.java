d9865fefaf6c07bc97424f2d7f778e72b840b8c5
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-05-11 22:15:07 -0300

    Serviço de Produto - implementando Casos de Uso no ProdutoService

--- produto-service/src/main/java/com/ms/rr/produto_service/application/port/input/ProdutoUseCase.java
+++ produto-service/src/main/java/com/ms/rr/produto_service/application/port/input/ProdutoUseCase.java
@@ -2,17 +2,17 @@ package com.ms.rr.produto_service.application.port.input;
 
 import com.ms.rr.produto_service.domain.model.ProdutoDomain;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface ProdutoUseCase {
 
     void salvar(ProdutoDomain produtoDomain);
 
-    Page<ProdutoDomain> buscarPorNome(String nome, Pageable pageable);
+    ProdutoDomain buscarPorId(Long id);
 
-    Page<ProdutoDomain> buscarTodos(Pageable pageable);
+    Page<ProdutoDomain> buscarTodosProdutos(int page, int size);
+
+    Page<ProdutoDomain> buscarProdutosPorNome(String nome, int page, int size);
 
     void excluir(Long id);
 }
--- produto-service/src/main/java/com/ms/rr/produto_service/domain/service/impl/ProdutoService.java
+++ produto-service/src/main/java/com/ms/rr/produto_service/domain/service/impl/ProdutoService.java
@@ -1,30 +1,46 @@
 package com.ms.rr.produto_service.domain.service.impl;
 
 import com.ms.rr.produto_service.application.port.input.ProdutoUseCase;
+import com.ms.rr.produto_service.application.port.output.ProdutoOutputPort;
 import com.ms.rr.produto_service.domain.model.ProdutoDomain;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService implements ProdutoUseCase {
+
+    private final ProdutoOutputPort produtoOutputPort;
+
+    public ProdutoService(ProdutoOutputPort produtoOutputPort) {
+        this.produtoOutputPort = produtoOutputPort;
+    }
+
     @Override
     public void salvar(ProdutoDomain produtoDomain) {
-
+        produtoOutputPort.save(produtoDomain);
     }
 
     @Override
-    public Page<ProdutoDomain> buscarPorNome(String nome, Pageable pageable) {
-        return null;
+    public ProdutoDomain buscarPorId(Long id) {
+        return produtoOutputPort.findById(id);
     }
 
     @Override
-    public Page<ProdutoDomain> buscarTodos(Pageable pageable) {
-        return null;
+    public Page<ProdutoDomain> buscarTodosProdutos(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return produtoOutputPort.findAll(pageable);
+    }
+
+    @Override
+    public Page<ProdutoDomain> buscarProdutosPorNome(String nome, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return produtoOutputPort.findByNomeContaining(nome, pageable);
     }
 
     @Override
     public void excluir(Long id) {
-
+        produtoOutputPort.delete(id);
     }
 }
