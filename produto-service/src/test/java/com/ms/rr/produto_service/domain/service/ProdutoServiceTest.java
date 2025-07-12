2ca112a76c3a493c5ce4878bc5762ff8c6cf6fc8
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-07-11 21:44:38 -0300

    Serviço de Produto Testes-teste da classe de serviço: testando caso de sucesso e caso de erro ao chamar método salvar

--- produto-service/src/test/java/com/ms/rr/produto_service/domain/service/ProdutoServiceTest.java
+++ produto-service/src/test/java/com/ms/rr/produto_service/domain/service/ProdutoServiceTest.java
@@ -1,6 +1,7 @@
 package com.ms.rr.produto_service.domain.service;
 
 import com.ms.rr.produto_service.domain.dto.in.CreateProduto;
+import com.ms.rr.produto_service.domain.exception.FornecedorNotFoundException;
 import com.ms.rr.produto_service.domain.model.FornecedorDomain;
 import com.ms.rr.produto_service.domain.model.ProdutoDomain;
 import com.ms.rr.produto_service.domain.port.output.FornecedorOutputPort;
@@ -17,6 +18,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Optional;
+
+import static com.ms.rr.produto_service.domain.exception.BaseErrorMessage.FORNECEDOR_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -58,8 +62,23 @@ class ProdutoServiceTest {
                     .verifyComplete();
 
             // ASSERT
-            verify(fornecedorOutputPort).findFornecedorById(123456789L);
+            verify(fornecedorOutputPort).findFornecedorById(createProduto.fornecedorId());
             verify(produtoOutputPort).save(any(ProdutoDomain.class));
+        }
+
+        @Test
+        void shouldThrowExceptionWhenFornecedorNotFound() {
+            // ARRANGE
+            when(fornecedorOutputPort.findFornecedorById(anyLong()))
+                    .thenReturn(Mono.error(new FornecedorNotFoundException(
+                            FORNECEDOR_NOT_FOUND.params(createProduto.fornecedorId().toString()).getMessage())));
+
+            // ACT & ASSERT
+            StepVerifier.create(produtoService.salvar(createProduto))
+                    .expectErrorMatches(throwable -> throwable instanceof FornecedorNotFoundException &&
+                            throwable.getMessage().equals(FORNECEDOR_NOT_FOUND
+                                    .params(createProduto.fornecedorId().toString()).getMessage()))
+                    .verify();
         }
     }
 }
