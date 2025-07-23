71259297b9e8dd3f7be56aa30f645d0d8f329344
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-07-22 21:12:49 -0300

    Serviço de Produto Testes de integração: testando caso de erro ao buscar fornecedor do recurso create; ajustanto retentativas na chamada para api externa, onde tem retentativas apenas para erros 5xx

--- produto-service/src/main/java/com/ms/rr/produto_service/adapter/output/client/PessoaWebClientAdapter.java
+++ produto-service/src/main/java/com/ms/rr/produto_service/adapter/output/client/PessoaWebClientAdapter.java
@@ -1,6 +1,7 @@
 package com.ms.rr.produto_service.adapter.output.client;
 
 import com.ms.rr.produto_service.domain.dto.out.FornecedorResponse;
+import com.ms.rr.produto_service.domain.exception.FornecedorNotFoundException;
 import com.ms.rr.produto_service.domain.model.FornecedorDomain;
 import com.ms.rr.produto_service.domain.port.output.FornecedorOutputPort;
 import org.springframework.http.HttpStatusCode;
@@ -9,7 +10,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
+import javax.naming.ServiceUnavailableException;
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+import static com.ms.rr.produto_service.domain.exception.BaseErrorMessage.FORNECEDOR_NOT_FOUND;
 
 @Component
 public class PessoaWebClientAdapter implements FornecedorOutputPort {
@@ -27,11 +32,14 @@ public class PessoaWebClientAdapter implements FornecedorOutputPort {
                 .retrieve()
                 .onStatus(
                         HttpStatusCode::is4xxClientError,
-                        response -> Mono.error(new RuntimeException("Fornecedor não encntrado, informar um id válido")))
+                        response -> Mono.error(new FornecedorNotFoundException(
+                                FORNECEDOR_NOT_FOUND.params(id.toString()).getMessage())))
                 .bodyToMono(FornecedorResponse.class)
                 .map(this::toDomain)
                 .timeout(Duration.ofSeconds(5))
-                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)));
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100))
+                .filter(throwable -> throwable instanceof ServiceUnavailableException
+                        || throwable instanceof TimeoutException));
     }
 
     private FornecedorDomain toDomain(FornecedorResponse fornecedor) {
--- produto-service/src/test/java/com/ms/rr/produto_service/integration/ProdutoResourceIT.java
+++ produto-service/src/test/java/com/ms/rr/produto_service/integration/ProdutoResourceIT.java
@@ -2,34 +2,41 @@ package com.ms.rr.produto_service.integration;
 
 import com.ms.rr.produto_service.domain.dto.out.ProdutoResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.ms.rr.produto_service.domain.exception.BaseErrorMessage.FORNECEDOR_NOT_FOUND;
 
 public class ProdutoResourceIT extends AbstractIntegrationTest {
 
     @BeforeEach
     void setupFornecedorStub() {
         configureFor(wiremock.getHost(), wiremock.getMappedPort(8080));
-
-        stubFor(get(urlPathMatching("/api/fornecedor/672978073"))
-                .willReturn(okJson("""
-            {
-              "id": 672978073,
-              "nome": "Fornecedor Teste",
-              "email": "teste@fornecedor.com",
-              "telefone": "61999999999",
-              "cpnj": "12345678000199",
-              "razaoSocial": "Fornecedor Teste Ltda"
-            }
-        """)));
     }
 
 
-    @Test
-    public void criarProdutos() {
-        String requestBody = """
+    @Nested
+    class CriarProdutoIT {
+        @Test
+        void createProduto() {
+
+            stubFor(get(urlPathMatching("/api/fornecedor/672978073"))
+                    .willReturn(okJson("""
+                    {
+                      "id": 672978073,
+                      "nome": "Fornecedor Teste",
+                      "email": "teste@fornecedor.com",
+                      "telefone": "61999999999",
+                      "cpnj": "12345678000199",
+                      "razaoSocial": "Fornecedor Teste Ltda"
+                    }
+                """)));
+
+            String requestBody = """
                 {
                     "nome":"Camisa Chelsea",
                     "descricao":"II Chelsea 24/25",
@@ -39,13 +46,56 @@ public class ProdutoResourceIT extends AbstractIntegrationTest {
                 }
                 """;
 
-        webTestClient.post()
-                .uri("/api/produto")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(requestBody)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody(ProdutoResponse.class);
+            webTestClient.post()
+                    .uri("/api/produto")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(requestBody)
+                    .exchange()
+                    .expectStatus().isCreated()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody(ProdutoResponse.class);
+        }
+
+        @Test
+        void errorFornecedorNotFound() {
+            Long fornecedorId = 763878074L;
+            LocalDateTime timestamp = LocalDateTime.now();
+
+            stubFor(get(urlEqualTo("/api/fornecedor/" + fornecedorId))
+                    .willReturn(aResponse()
+                            .withStatus(404)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody(String.format("""
+                                    {
+                                        "status": 404,
+                                        "message": "%s",
+                                        "timestamp": "%s"
+                                    }
+                                    """,
+                                    FORNECEDOR_NOT_FOUND.params(String.valueOf(fornecedorId)).getMessage(),
+                                    timestamp
+                            ))));
+
+
+            String requestBody = String.format("""
+                {
+                    "nome":"Camisa Chelsea",
+                    "descricao":"II Chelsea 24/25",
+                    "categoria":"Roupas",
+                    "preco":"280.90",
+                    "fornecedorId": %s
+                }
+                """, fornecedorId);
+
+            webTestClient.post()
+                    .uri("/api/produto")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(requestBody)
+                    .exchange()
+                    .expectStatus().isNotFound()
+                    .expectBody()
+                    .jsonPath("$.status").isEqualTo("404")
+                    .jsonPath("$.message").isEqualTo(FORNECEDOR_NOT_FOUND.params(fornecedorId.toString()).getMessage());
+        }
     }
 }
