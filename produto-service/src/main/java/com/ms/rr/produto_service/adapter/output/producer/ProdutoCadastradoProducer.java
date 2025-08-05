f2486b2b0f078218528338ddf0a3f53a75901012
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-08-05 20:28:00 -0300

    Serviço de Produto: criando producer kafka

--- produto-service/src/main/java/com/ms/rr/produto_service/adapter/output/producer/ProdutoCadastradoProducer.java
+++ produto-service/src/main/java/com/ms/rr/produto_service/adapter/output/producer/ProdutoCadastradoProducer.java
@@ -1,11 +1,30 @@
 package com.ms.rr.produto_service.adapter.output.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ms.rr.produto_service.domain.model.ProdutoDomain;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProdutoCriadoProducer {
+public class ProdutoCadastradoProducer {
 
     @Value("${topicos.produto-cadastrado.request.topic}")
-    private String topic;
+    private String topicoProdutoCadastrado;
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    private final ObjectMapper objectMapper;
+
+    public ProdutoCadastradoProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public String sendMessage(ProdutoDomain produtoDomain) throws JsonProcessingException {
+        String orderAsMessage = objectMapper.writeValueAsString(produtoDomain);
+        kafkaTemplate.send(topicoProdutoCadastrado, orderAsMessage);
+        return orderAsMessage;
+    }
 }
--- produto-service/src/main/java/com/ms/rr/produto_service/config/KafkaConfig.java
+++ produto-service/src/main/java/com/ms/rr/produto_service/config/KafkaConfig.java
@@ -1,2 +1,47 @@
-package com.ms.rr.produto_service.config;public class KafkaConfig {
+package com.ms.rr.produto_service.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Value("${topicos.produto-cadastrado.request.topic}")
+    private String produtoCriadoTopic;
+
+    public KafkaConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = kafkaProperties
+                .buildProducerProperties();
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic produtoCriadoRequestTopicBuilder() {
+        return TopicBuilder
+                .name(produtoCriadoTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
 }
