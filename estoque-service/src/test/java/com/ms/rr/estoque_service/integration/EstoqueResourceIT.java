4d7bd01ab51938bb82aeee57ecdc48d07a0dc613
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-09-16 21:00:17 -0300

    Testes Integração Serviço Estoque - testando recursos de buscar todos e buscar todos por tipo com paginação

--- estoque-service/src/test/java/com/ms/rr/estoque_service/integration/EstoqueResourceIT.java
+++ estoque-service/src/test/java/com/ms/rr/estoque_service/integration/EstoqueResourceIT.java
@@ -2,6 +2,7 @@ package com.ms.rr.estoque_service.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ms.rr.estoque_service.adapter.input.handler.ApiError;
+import com.ms.rr.estoque_service.domain.dto.out.ApiResponse;
 import com.ms.rr.estoque_service.domain.dto.out.EstoqueResponse;
 import com.ms.rr.estoque_service.domain.exception.ProdutoNotFoundException;
 import com.ms.rr.estoque_service.domain.port.input.EstoqueUseCase;
@@ -18,12 +19,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import static com.ms.rr.estoque_service.domain.exception.BaseErrorMessage.PRODUTO_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -91,6 +94,64 @@ public class EstoqueResourceIT extends AbstractIntegrationTest {
 
             assertEquals(response.status(), apiError.status());
             assertEquals(response.description(), apiError.description());;
+        }
+    }
+
+    @Nested
+    class FindAllWithPagination {
+
+        @Test
+        void shouldFindAllWithPaginationSuccessfully() throws Exception {
+            int page = 0;
+            int pageSize = 3;
+            var estoqueResponsePagination = EstoqueFactory.createWithPageEstoqueResponse();
+
+            var mvcResult = mockMvc.perform(get("/api/estoque")
+                    .param("page", Integer.toString(page))
+                    .param("pageSize", Integer.toString(pageSize))
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.summary.totalOnEstoque").value(1))
+                    .andExpect(jsonPath("$.data").isArray())
+                    .andExpect(jsonPath("$.pagination.page").value(0))
+                    .andExpect(jsonPath("$.pagination.pageSize").value(3))
+                    .andReturn();
+
+            String responseBody = mvcResult.getResponse().getContentAsString();
+            var response = objectMapper.readValue(responseBody, ApiResponse.class);
+
+            assertEquals(response.data().size(), 1);
+        }
+    }
+
+    @Nested
+    class FindAllByTipoWithPagination {
+
+        @Test
+        void shouldFindAllByTipoWithPaginationSuccessfully() throws Exception {
+            int page = 0;
+            int pageSize = 3;
+            var tipoProduto = "Roupa";
+            var totalEsperado = BigDecimal.valueOf(2);
+
+            var mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(
+                                    "/api/estoque/tipo-produto/{tipoProduto}", tipoProduto)
+                            .param("page", Integer.toString(page))
+                            .param("pageSize", Integer.toString(pageSize))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.summary.totalOnEstoque").value(2))
+                    .andExpect(jsonPath("$.data").isArray())
+                    .andExpect(jsonPath("$.pagination.page").value(0))
+                    .andExpect(jsonPath("$.pagination.pageSize").value(3))
+                    .andReturn();
+
+
+            String responseBody = mvcResult.getResponse().getContentAsString();
+            var response = objectMapper.readValue(responseBody, ApiResponse.class);
+
+            assertEquals(response.data().size(), 1);
+            assertEquals(response.summary().get("totalOnEstoque"), totalEsperado.intValueExact());
         }
     }
 }
