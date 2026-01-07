b98aade609bfbaf863be662f8180f6a05057724d
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2026-01-06 22:06:20 -0300

    Serviço de Pessoas - Refatoração: melhorando testes SQLFornecedorRepository

--- pessoa-service/src/test/java/com/ms/rr/pessoa_service/api/output/FornecedorOutputPortTest.java
+++ pessoa-service/src/test/java/com/ms/rr/pessoa_service/api/output/FornecedorOutputPortTest.java
@@ -1,5 +1,6 @@
 package com.ms.rr.pessoa_service.api.output;
 
+import com.ms.rr.pessoa_service.adapter.output.persistence.entity.Fornecedor;
 import com.ms.rr.pessoa_service.domain.model.FornecedorDomain;
 import com.ms.rr.pessoa_service.domain.port.output.FornecedorOutputPort;
 import com.ms.rr.pessoa_service.factory.FornecedorDomainFactory;
@@ -7,15 +8,21 @@ import com.ms.rr.pessoa_service.infrastructure.adapter.AbstractContainerTest;
 import org.instancio.Instancio;
 import org.instancio.Select;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class FornecedorOutputPortTest extends AbstractContainerTest {
 
     public abstract FornecedorOutputPort getFornecedorOutputPort();
+
+    @Autowired
+    TestEntityManager testEntityManager;
 
     @Test
     void save() {
@@ -36,10 +43,48 @@ public abstract class FornecedorOutputPortTest extends AbstractContainerTest {
     void findByCnpj() {
         FornecedorDomain fornecedorDomain1 = FornecedorDomainFactory.createFornecedorDomain();
 
-        getFornecedorOutputPort().save(fornecedorDomain1);
+        testEntityManager.persist(Fornecedor.fromDomain(fornecedorDomain1));
 
-        FornecedorDomain fornecedor = getFornecedorOutputPort().findFornecedorByCnpj(fornecedorDomain1.cnpj());
+        FornecedorDomain fornecedorRecuperado = getFornecedorOutputPort().findFornecedorByCnpj(fornecedorDomain1.cnpj());
 
-        assertEquals("50.095.037/0001-19", fornecedor.cnpj());;
+        assertEquals("50.095.037/0001-19", fornecedorRecuperado.cnpj());
+        assertNotNull(fornecedorRecuperado);
+    }
+
+    @Test
+    void findById() {
+        FornecedorDomain fornecedorDomain = FornecedorDomainFactory.createFornecedorDomain();
+
+        testEntityManager.persist(Fornecedor.fromDomain(fornecedorDomain));
+
+        Optional<FornecedorDomain> fornecedorRecuperado = getFornecedorOutputPort().findById(fornecedorDomain.id());
+
+        assertTrue(fornecedorRecuperado.isPresent());
+        assertEquals("Goku", fornecedorRecuperado.get().nome());
+        assertThat(fornecedorRecuperado).contains(fornecedorDomain);
+    }
+
+    @Test
+    void findAll() {
+        FornecedorDomain fornecedorDomain1 = Instancio.of(FornecedorDomain.class)
+                .set(Select.field("cnpj"), "02.925.356/0001-61")
+                .set(Select.field("email"), "test1@gmail.com")
+                .set(Select.field("telefone"), "(61) 25517852")
+                .create();
+        FornecedorDomain fornecedorDomain2 = Instancio.of(FornecedorDomain.class)
+                .set(Select.field("cnpj"), "58.192.226/0001-11")
+                .set(Select.field("email"), "test2@gmail.com")
+                .set(Select.field("telefone"), "(61) 37491474")
+                .create();
+
+        List<FornecedorDomain> fornecedores = List.of(fornecedorDomain1, fornecedorDomain2);
+
+        fornecedores
+                .forEach(fornecedorDomain ->
+                        testEntityManager.persist(Fornecedor.fromDomain(fornecedorDomain)));
+
+        List<FornecedorDomain> fornecedoreRecupreados = getFornecedorOutputPort().findAll();
+
+        assertEquals(fornecedores.size(), fornecedoreRecupreados.size());
     }
 }
