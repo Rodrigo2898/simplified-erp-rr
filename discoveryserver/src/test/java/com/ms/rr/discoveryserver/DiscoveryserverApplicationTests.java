dee1a90687694114114ec593762e540f86b73f82
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2026-02-11 21:36:38 -0300

    Conterinerização: Correção de propriedades

---
+++ discoveryserver/src/test/java/com/ms/rr/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.rr.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
